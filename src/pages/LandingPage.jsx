c9b33e7be39d834a2811eb38abefd622dfcc24bb
ShreeKalyaaneB <shreekalyaane@gmail.com>
2023-02-17 15:05:06 +0530

    Login working

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,22 +10,22 @@ import Register from '../components/Modal/Register';
 function LandingPage(props) {
 const history=useNavigate()
 const [modalIsOpen, setModalisOpen] = useState(false);
-
+const [RegistermodalIsOpen, setRegisterModalisOpen] = useState(false);
   function login(){
     setModalisOpen(true);
   }
   function register(){
-    setModalisOpen(true);
+    setRegisterModalisOpen(true);
   }
-
+  
 
   return (
     <div>
       <h1>Hi selva please work on the landing page,thank you</h1>
       <button className='login-btn' onClick={login}>Login</button> 
-      {modalIsOpen && <Login onLogin={login}/>} 
       <button className='Register-btn' onClick={register}>Register</button>
-      {modalIsOpen && <Register onRegister={register}/>}
+      {modalIsOpen && <Login onLogin={login}/>} 
+      {RegistermodalIsOpen && <Register onRegister={register}/>}
     </div>
   )
 }
