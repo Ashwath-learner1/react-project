279ad7e06a75f226b4ec564e8105f42044e1c249
Ashwath-learner1 <ashwathkumaruumapathy@gmail.com>
2023-02-21 12:58:59 +0530

    nothing

--- src/pages/CustomerDboard.jsx
+++ src/pages/CustomerDboard.jsx
@@ -1,12 +1,13 @@
 import { React, useEffect, useState } from "react";
-import Sidebar from "../components/Sidebar";
+// import Sidebar from "../components/Sidebar";
 // import Payment from '../components/Payment';
+import Header from "../components/Header/Header";
 
 function App() {
   
   return (
     <div>
-      <Sidebar />
+      <Header />
     </div>
   );
 }
