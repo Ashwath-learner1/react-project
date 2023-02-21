7191491d72d365df1e87831c65cb4609d724a011
SELVAKUMAR T <selva1712098@gmail.com>
2023-02-21 12:19:33 +0530

    created header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import './Header.css'
-function Header() {
+import './Header.css';
+
+function Navbar() {
   return (
-    <div className='header'>
-      <h1>BlockFlexi</h1>
-      
-    </div>
+    <nav className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__logo">
+          Block Flexi
+        </div>
+        <ul className="navbar__links">
+          <li className="navbar__item">
+            <a href="/" className="navbar__link">Home</a>
+          </li>
+          <li className="navbar__item">
+            <a href="/about" className="navbar__link">About</a>
+          </li>
+          <li className="navbar__item">
+            <a href="/paygold" className="navbar__link">Paygold</a>
+          </li>
+          <li className="navbar__item">
+            <a href="/withdraw" className="navbar__link">Withdraw</a>
+          </li>
+          <li className="navbar__item">
+            <a href="/contact" className="navbar__link">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
-export default Header;
+export default Navbar;
